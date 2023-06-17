ef4812736a14cacc308845b0f1c3591e20b61637
DoggyyyyyYY <136892062+DoggyyyyyYY@users.noreply.github.com>
2023-06-17 11:41:44 +0200

    config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.ShowerCoords = vector3(-633.6021, 944.3239, 243.9170) -- Change these coordinates to the desired shower location
+Config.ShowerHeading = 351.5461 -- Change this value to the desired player heading in the shower
+Config.ShowerControl = 38 -- Change this control value to the desired input for triggering the shower (see https://docs.fivem.net/docs/game-references/controls/)
+
+Config.MarkerType = 2 -- Change this value to the desired marker type (see https://docs.fivem.net/docs/game-references/markers/)
+Config.MarkerSize = { x = 1.3, y = 1.3, z = 1.3 } -- Change this value to adjust the size of the marker
+Config.MarkerColor = { r = 0, g = 128, b = 255, a = 150 } -- Change this value to adjust the color and opacity of the marker
+Config.MarkerDistance = 5.0 -- Change this value to adjust the distance at which the marker becomes visible
+Config.ShowerDuration = 7000
